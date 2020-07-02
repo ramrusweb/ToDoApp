a99034b9af648fa2dbde36dbc8119309ca806d40
rrm1 <rustamramazanovworkmail@gmail.com>
2020-07-02 16:43:34 +0300

    (add): added model; ref.

--- ToDoApp/MainWindow.xaml.cs
+++ ToDoApp/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ToDoApp.Models;
 
 namespace ToDoApp
 {
@@ -20,9 +9,22 @@ namespace ToDoApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BindingList<TodoModel> _todoData;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _todoData = new BindingList<TodoModel>()
+            {
+                new TodoModel() {Text = "test"},
+                new TodoModel() {Text = "dfsdfsd"}
+            };
+
+            dgTodoList.ItemsSource = _todoData;
         }
     }
 }
---
+++ ToDoApp/Models/TodoModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ToDoApp.Models
+{
+    public class TodoModel
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        private bool _isDone;
+        private string _text;
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set { _isDone = value; }
+        }
+
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+    }
+}
